bd298cd97019063617cdb1ee433e2524683c3de0
jcowen-Berklee <jcowen@berklee.edu>
2017-04-07 23:35:47 -0400

    Original Assignment Submission

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -10,13 +10,36 @@ public class ApplyForceInRandomDirection : MonoBehaviour
 	public float torqueAmount = 10.0f;
 	public ForceMode forceMode;
 
+	private bool rollComplete = false;
+
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetButtonDown(buttonName))
+	//	if(Input.GetButtonDown(buttonName))
+		if (Input.GetButtonDown (buttonName)) 
+		
 		{
-			GetComponent<Rigidbody>().AddForce(Random.onUnitSphere*forceAmount,forceMode);
-			GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
+			InvokeRepeating ("RollSim", 0, 20);
+		}
+
+		if(GetComponent<Rigidbody>().IsSleeping() && !rollComplete)
+		{
+			rollComplete = true;
+			Debug.Log("Die roll complete, die is at rest");
+
+		}
+		else if(!GetComponent<Rigidbody>().IsSleeping())
+		{
+			rollComplete = false;
+		}
+	}
+
+	void RollSim()
+	{
+		if (rollComplete = true)
+		{
+			GetComponent<Rigidbody> ().AddForce (Random.onUnitSphere * forceAmount, forceMode);
+			GetComponent<Rigidbody> ().AddTorque (Random.onUnitSphere * torqueAmount, forceMode);
 		}
 	}
 }
